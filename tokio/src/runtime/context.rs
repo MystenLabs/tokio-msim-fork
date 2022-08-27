c3e4b308935da1d752202773139ceafab20efecc
Mark Logan <mark@mystenlabs.com>
2022-08-27 16:32:39 -0700

    Expose the fast_rng so it can be reset

--- tokio/src/runtime/context.rs
+++ tokio/src/runtime/context.rs
@@ -3,8 +3,8 @@ use crate::task::coop;
 
 use std::cell::Cell;
 
-#[cfg(any(feature = "rt", feature = "macros"))]
-use crate::util::rand::FastRand;
+#[cfg(any(feature = "rt", feature = "macros", feature = "time"))]
+use crate::util::rand::{FastRand, RngSeed};
 
 cfg_rt! {
     mod blocking;
@@ -128,6 +128,15 @@ pub(crate) fn thread_rng_n(n: u32) -> u32 {
         let ret = rng.fastrand_n(n);
         ctx.rng.set(Some(rng));
         ret
+    })
+}
+
+#[cfg(any(feature = "rt", feature = "macros"))]
+pub(crate) fn reset_rng(seed: RngSeed) {
+    CONTEXT.with(|ctx| {
+        let mut rng = ctx.rng.get().unwrap_or_else(FastRand::new);
+        rng.replace_seed(seed);
+        ctx.rng.set(Some(rng));
     })
 }
 
